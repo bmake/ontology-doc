c1e7f70e6b734a2533a9e2d4641c80a9fb92c14c
Jonas <jetschni@gmail.com>
2016-03-08 21:16:34 +0100

    Header updated to text/plain; charset=utf-8

--- ttlToRdf.php
+++ ttlToRdf.php
@@ -10,6 +10,9 @@ $response = $client->request('GET', 'rdfxml/' . $ttl );
 header('HTTP/ ' . $response->getStatusCode() .' ' . $response->getReasonPhrase());
 //http_response_code($response->getStatusCode());
 //header('Content-Type: ' . implode( $response->getHeader('Content-Type')));
+header('Content-Type: text/plain; charset=utf-8');
+
+
 
 echo $response->getBody();
 
