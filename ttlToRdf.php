2290a48212694ef66e5edacc0f71b65127fc814a
Jonas <jetschni@gmail.com>
2016-03-08 21:48:58 +0100

    .htaccess added with .rdf support

--- ttlToRdf.php
+++ ttlToRdf.php
@@ -2,19 +2,33 @@
 
 require_once('vendor/autoload.php');
 
-$ttl = $_GET['ttl'];
+$ontology = $_GET['ontology'];
+
+switch(strtolower($ontology)){
+    case 'itcat':
+        //https://raw.githubusercontent.com/ITcatalog/ITcat/master/Ontology/SchemaGraph.ttl
+        $ttl = 'https://raw.githubusercontent.com/ITcatalog/ITcat/master/Ontology/'.$_GET['file'].'.ttl';
+        break;
+
+    case 'edugraph':
+        //https://raw.githubusercontent.com/EduGraph/bise-ontology/master/bise_schema.ttl
+        $ttl = 'https://raw.githubusercontent.com/EduGraph/bise-ontology/master/'.$_GET['file'].'.ttl';
+        break;
+}
+
 
 $client = new GuzzleHttp\Client(['base_uri' => 'http://any23.org/']);
 $response = $client->request('GET', 'rdfxml/' . $ttl );
 
+
+
+// Response
 header('HTTP/ ' . $response->getStatusCode() .' ' . $response->getReasonPhrase());
 //http_response_code($response->getStatusCode());
 //header('Content-Type: ' . implode( $response->getHeader('Content-Type')));
 header('Content-Type: text/plain; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 header('Accept-Ranges: bytes');
-
-
 
 echo $response->getBody();
 
