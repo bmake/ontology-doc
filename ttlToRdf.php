60eefe1171f346448a72fd051571f6c5ffff3895
Jonas <jetschni@gmail.com>
2016-03-08 21:11:28 +0100

    Init commit

---
+++ ttlToRdf.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once('vendor/autoload.php');
+
+$ttl = $_GET['ttl'];
+
+$client = new GuzzleHttp\Client(['base_uri' => 'http://any23.org/']);
+$response = $client->request('GET', 'rdfxml/' . $ttl );
+
+header('HTTP/ ' . $response->getStatusCode() .' ' . $response->getReasonPhrase());
+//http_response_code($response->getStatusCode());
+header('Content-Type: ' . implode( $response->getHeader('Content-Type')));
+
+echo $response->getBody();
+
+//http://www.essepuntato.it/lode/lang=de/https://raw.githubusercontent.com/ITcatalog/ITcat/master/Ontology/SchemaGraph.rdf
